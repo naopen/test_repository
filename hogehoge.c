d8255b790163067ba0f1e3f09d332d79b90b67e2
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2024-12-30 17:12:57 +0900

    merge

--- hogehoge.c
+++ hogehoge.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 17:10:11 by nkannan           #+#    #+#             */
-/*   Updated: 2024/12/30 17:10:34 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/12/30 17:12:47 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
-
-	if (!lst || !new)
+	
+	if (!lst || !new) //////////////////////
 		return ;
 	if (!*lst)
 	{
