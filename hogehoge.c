6926d2539f4222069195dca77e7f5bc21a6eb76b
naopen <naokizssm@gmail.com>
2024-12-30 17:13:04 +0900

    Update comment timestamp and add inline comment in ft_lstadd_back function

--- hogehoge.c
+++ hogehoge.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 17:10:11 by nkannan           #+#    #+#             */
-/*   Updated: 2024/12/30 17:10:34 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/12/30 17:12:56 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!lst || !new)
+	if (!lst || !new) // nyan
 		return ;
 	if (!*lst)
 	{
